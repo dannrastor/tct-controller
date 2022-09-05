a650fc3f88fbfd97dc807b43f7d88f7d4a897b68
dannrastor <dannraptor@gmail.com>
2022-09-05 17:40:00 +0200

    laser stability update

--- measurements/laser_stability.py
+++ measurements/laser_stability.py
@@ -1,5 +1,7 @@
 from core import *
+import pickle
 from measurements.async_worker import AsyncWorker
+import numpy as np
 
 
 class LaserStabilityWorker(AsyncWorker):
@@ -7,24 +9,42 @@ class LaserStabilityWorker(AsyncWorker):
 
     def action(self):
 
-        total_steps = 10
+        xrange = range(16700, 17500, 5)  # range(*self.settings['xrange'])
+
+        counter_max = 51
+        total_steps = len(xrange) * counter_max
+
         core.measurement_state = 0, total_steps
+        self.result = {}
+        current_steps = 0
 
         # Main loop over measurement steps
-        for i in range(total_steps):
-            # Check for abort request
-            if QThread.currentThread().isInterruptionRequested():
-                # If received, break measurement loop
-                break
+        for counter in range(counter_max):  # just as an example
+            self.result[counter] = {}
+            for x in xrange:
+                # creates an empty dictionary
+                # Check for abort request every iteration
+                if QThread.currentThread().isInterruptionRequested():
+                    # If received, break measurement loop
+                    break
 
-            # Update that with relevant values every step, so the progress bar and time estimate work
-            core.measurement_state = i + 1, total_steps
+                if core.motors is not None:
+                    core.motors.move_abs('x', x)
 
-            # Dummy action
-            time.sleep(1)
+                time.sleep(0.2)
+                while core.motors.is_moving('x'):
+                    time.sleep(0.05)
 
-        self.finalize()
+                time.sleep(0.5)
 
-    def finalize(self):
-        # Stuff to do at the end of a run, as a separate function for convenience
-        pass
+                if core.oscilloscope is not None:
+                    t, v = core.oscilloscope.get_waveform(2)
+                    self.result[counter][x] = np.sum(v), time.time()  # then when drawing the plots enquire into the counter value
+                    # Update that with relevant values every step, so the progress bar and time estimate work
+                core.measurement_state = current_steps, total_steps
+                current_steps += 1
+                # Dummy action
+                time.sleep(1)
+
+        with open('/home/drastorg/tct/data/stability.pickle', 'wb') as f:
+            pickle.dump(self.result, f)

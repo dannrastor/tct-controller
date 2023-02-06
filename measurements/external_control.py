69422fbb5490d0d0cb039d830c1c05fab05451f1
dannrastor <dannraptor@gmail.com>
2023-02-06 10:01:30 +0100

    Fiddling with server

--- measurements/external_control.py
+++ measurements/external_control.py
@@ -2,6 +2,7 @@ from core import *
 from measurements.async_worker import AsyncWorker
 import socket
 
+
 class ExternalControlWorker(AsyncWorker):
 
     description = 'Remote control via network'
@@ -16,17 +17,32 @@ class ExternalControlWorker(AsyncWorker):
         server_socket.bind((host, port))  # bind host address and port together
 
         # configure how many client the server can listen simultaneously
-        server_socket.listen(1)
-        conn, address = server_socket.accept()  # accept new connection
+        server_socket.settimeout(1)
+        conn, address = None, None
+
+        logging.info("Waiting for a client...")
+        while True:
+            if QThread.currentThread().isInterruptionRequested():
+                return
+            try:
+                server_socket.listen(1)
+                conn, address = server_socket.accept()  # accept new connection
+                break
+            except socket.timeout:
+                pass
+
         logging.info("Connection from: " + str(address))
+
         while True:
             if QThread.currentThread().isInterruptionRequested():
                 break
             # receive data stream. it won't accept data packet greater than 1024 bytes
-            data = conn.recv(1024).decode()
-            if not data:
-                # if data is not received break
-                break
-            logging.info("from connected user: " + str(data))
+            try:
+                data = conn.recv(1024).decode()
+                logging.info("from connected user: " + str(data))
+            except socket.timeout:
+                pass
+
+
 
         conn.close()  # close the connection
